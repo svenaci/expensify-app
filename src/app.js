6815c764d026002199bef78f58703b94d2868ad5
svenaci <steffan_v@hotmail.com>
2023-11-16 12:27:21 -0500

    add routes configuration and dummy components

--- src/app.js
+++ src/app.js
@@ -1,10 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
-function Hello(props) {
-  return <h1>Hello World!</h1>;
-}
+const ExpenseDashboardPage = () => (
+  <div>Ths is from my dashboard component</div>
+);
 
-ReactDOM.render(<Hello />, document.getElementById("app"));
+const AddExpensePage = () => <div>Ths is my add expense component</div>;
+const EditExpensePage = () => <div>Ths is my edit expense component</div>;
+const HelpPage = () => <div>Ths is my help component</div>;
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={ExpenseDashboardPage} exact={true} />
+      <Route path="/create" component={AddExpensePage} />
+      <Route path="/edit" component={EditExpensePage} />
+      <Route path="/help" component={HelpPage} />
+    </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById("app"));
