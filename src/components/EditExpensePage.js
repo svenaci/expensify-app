d9c2fff006b48d7428a2cdd0d7800e580664fff1
svenaci <steffan_v@hotmail.com>
2023-11-16 20:56:13 -0500

    adding params of id to edit expense page

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,4 +1,4 @@
 import React from "react";
-const EditExpensePage = () => <div>Ths is my edit expense component</div>;
+const EditExpensePage = () => <div>Ths is my edit expense componen of</div>;
 
 export default EditExpensePage;
