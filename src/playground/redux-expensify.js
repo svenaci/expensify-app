7fe6a0da83a807108d2093744cfb0dbdbba787ab
svenaci <steffan_v@hotmail.com>
2023-11-19 17:17:46 -0500

    add filtering by text, startDate and endDate

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -127,36 +127,53 @@ const store = createStore(
   })
 );
 
+// get visible expense
+const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
+  return expenses.filter((expense) => {
+    const startDateMatch =
+      typeof startDate !== "number" || expense.createdAt >= startDate;
+    const endDateMatch =
+      typeof endDate !== "number" || expense.createdAt <= endDate;
+    const textMatch = expense.description
+      .toLowerCase()
+      .includes(text.toLowerCase());
+    return startDateMatch && endDateMatch && textMatch;
+  });
+};
 store.subscribe(() => {
-  console.log(store.getState());
+  const state = store.getState();
+  const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+  console.log(visibleExpenses);
 });
 
-// const expenseOne = store.dispatch(
-//   addExpense({
-//     description: "Rent",
-//     amount: 100,
-//   })
-// );
+const expenseOne = store.dispatch(
+  addExpense({
+    description: "rent",
+    amount: 100,
+    createdAt: 1000,
+  })
+);
 
-// const expenseTwo = store.dispatch(
-//   addExpense({
-//     description: "date",
-//     amount: 150,
-//   })
-// );
+const expenseTwo = store.dispatch(
+  addExpense({
+    description: "Coffee",
+    amount: 150,
+    createdAt: -1000,
+  })
+);
 
 // store.dispatch(removeExpense({ id: expenseOne.expense.id }));
 // store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }));
 
-// store.dispatch(setTextFilter("rent"));
+store.dispatch(setTextFilter("ffee"));
 // store.dispatch(setTextFilter());
 
 // store.dispatch(sortByAmount());
 // store.dispatch(sortByDate());
 
-store.dispatch(setStartDate(125));
-store.dispatch(setStartDate());
-store.dispatch(setEndDate(1250));
+// store.dispatch(setStartDate(125));
+// store.dispatch(setStartDate());
+// store.dispatch(setEndDate(1250));
 
 const demoState = {
   expenses: [
@@ -175,13 +192,3 @@ const demoState = {
     endDate: undefined,
   },
 };
-
-const user = {
-  name: "Jen",
-  age: 24,
-};
-
-console.log({
-  ...user,
-  location: "Philly",
-});
