d4ed734f5d45a2415052fd720d46fc4828f560b1
svenaci <steffan_v@hotmail.com>
2023-11-28 13:28:12 -0500

    implement edit expense page

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,4 +1,29 @@
 import React from "react";
-const EditExpensePage = () => <div>Ths is my edit expense componen of</div>;
+import { connect } from "react-redux";
+import ExpenseForm from "./ExpenseForm";
+import { editExpense } from "../actions/expenses";
 
-export default EditExpensePage;
+const EditExpensePage = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <ExpenseForm
+        expense={props.expense}
+        onSubmit={(expense) => {
+          props.dispatch(editExpense(props.expense.id, expense));
+          props.history.push("/");
+        }}
+      />
+    </div>
+  );
+};
+
+const mapStateToProps = (state, props) => {
+  return {
+    expense: state.expenses.find((expense) => {
+      return expense.id === props.match.params.id;
+    }),
+  };
+};
+
+export default connect(mapStateToProps)(EditExpensePage);
