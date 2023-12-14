ef92b9ae03278f79bd6cfe567b653ea98598f80f
svenaci <steffan_v@hotmail.com>
2023-12-14 12:04:41 -0500

    hide firebase secrets and setup for test

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,13 +1,12 @@
 import * as firebase from "firebase";
-import expenses from "../test/fixtures/expenses";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAkZHFuV9qZvyfqNWR5rFfkf-WPnDH6CmQ",
-  authDomain: "expense-7bf6b.firebaseapp.com",
-  databaseURL: "https://expense-7bf6b-default-rtdb.firebaseio.com",
-  projectId: "expense-7bf6b",
-  storageBucket: "expense-7bf6b.appspot.com",
-  messagingSenderId: "1090810973003",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
   appId: "1:1090810973003:web:51b518af75d2b5cb45f6ce",
   measurementId: "G-L7FZFBSGJN",
 };
