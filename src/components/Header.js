0b76fca7935b44592587ff1f07c739b90181d745
svenaci <steffan_v@hotmail.com>
2024-01-08 11:06:44 -0500

    add logout action and button to logout users

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { startLogout } from "../actions/auth";
 
-const Header = () => (
+export const Header = ({ startLogout }) => (
   <header className="header">
     <div className="content-container">
       <div className="header__content">
@@ -9,9 +11,13 @@ const Header = () => (
           <h1>Expense App</h1>
         </Link>
         {/* <button className="button button--logout">Logout</button> */}
+        <button onClick={startLogout}>Logout</button>
       </div>
     </div>
   </header>
 );
 
-export default Header;
+const mapDispatchToProps = (dispatch) => ({
+  startLogout: () => dispatch(startLogout()),
+});
+export default connect(undefined, mapDispatchToProps)(Header);
